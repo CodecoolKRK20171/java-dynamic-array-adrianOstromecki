5f072d3a126b1dd11411801e4fb2bfbfe5e8ec43
adrianOstromecki <bloodrian@gmail.com>
2019-01-16 13:59:51 +0100

    implement add method and satisfy add test

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,24 +2,56 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
+    private int[] array;
     private int size;
 
     public DynamicIntArray() {
+        array = new int[0];
+        size = 0;
     }
 
     public DynamicIntArray(int size) {
         this.size = size;
+        array = new int[size];
     }
 
-    public void add(int value){
+    public void add(int value) {
+        if (size >= array.length) {
+            expandArray();
+        }
+        array[size++] = value;
+    }
+
+    private void expandArray() {
+        int[] expandedArray = new int[size + 5];
+        copyArray(expandedArray, array);
+        array = expandedArray;
+    }
+
+    private void copyArray(int[] newArray, int[] oldArray) {
+        int size = oldArray.length;
+
+        for (int i=0; i<size; i++) {
+            newArray[i] = oldArray[i];
+        }
+    }
+
+    public void remove(int value) {
 
     }
 
-    public void remove(int value){
+    public void insert(int index, int value) {
 
     }
 
-    public void insert(int index, int value){
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
 
+        for (int i=0; i<size; i++) {
+            sb.append(String.format(" %d", array[i]));
+        }
+
+        return sb.toString();
     }
 }
