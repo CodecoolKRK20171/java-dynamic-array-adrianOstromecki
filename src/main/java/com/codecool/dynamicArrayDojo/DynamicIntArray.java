a9a5fada8183eef40be1329a6df03ef071af34a1
adrianOstromecki <bloodrian@gmail.com>
2019-01-16 13:32:35 +0100

    add empty methods body to satisfy compilerand pass initialization test

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,5 +2,24 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
+    private int size;
 
+    public DynamicIntArray() {
+    }
+
+    public DynamicIntArray(int size) {
+        this.size = size;
+    }
+
+    public void add(int value){
+
+    }
+
+    public void remove(int value){
+
+    }
+
+    public void insert(int index, int value){
+
+    }
 }
